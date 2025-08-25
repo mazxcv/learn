0755dd1ef980fd35bab36d49f2614063c147337e
Maxim Ugrin <mazxccc@gmail.com>
2025-08-25 14:50:09 +0300

    201. Bitwise AND of Numbers Range

---
+++ algorithm/bitManipulation/leet/bitwiseAndOfNumberRange_test.go
@@ -0,0 +1,50 @@
+package leet
+
+import "testing"
+
+func Test_rangeBitwiseAnd(t *testing.T) {
+	type args struct {
+		left  int
+		right int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// left = 5, right = 7
+		{
+			name: "1",
+			args: args{
+				left:  5,
+				right: 7,
+			},
+			want: 4,
+		},
+		// left = 0, right = 0
+		{
+			name: "2",
+			args: args{
+				left:  0,
+				right: 0,
+			},
+			want: 0,
+		},
+		// left = 1, right = 2147483647
+		{
+			name: "3",
+			args: args{
+				left:  1,
+				right: 2147483647,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rangeBitwiseAnd(tt.args.left, tt.args.right); got != tt.want {
+				t.Errorf("rangeBitwiseAnd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
